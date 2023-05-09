52bd9566156a7f92fd66cae96e2404976c0ec4cf
WassCodeur <wachioubouraima56@gmail.com>
2023-05-09 17:54:36 +0000

    fix error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,15 @@ from fabric.api import *
 from datetime import datetime
 import os
 
-
 env.hosts = ['localhost']
 
-do_pack():
-    """ Create archive"""
+
+def do_pack():
+    """ Create archive """
     try:
 
-        curent = datetime.now().srtftime('%Y%m%d%H%M%S')
-        archive = "versions/web_static_{}.tgz".format(curent)
+        current = datetime.now().strftime('%Y%m%d%H%M%S')
+        archive = "versions/web_static_{}.tgz".format(current)
         local("mkdir -p versions")
         local("tar -cvzf {} web_static".format(archive))
         size = os.path.getsize(archive)
