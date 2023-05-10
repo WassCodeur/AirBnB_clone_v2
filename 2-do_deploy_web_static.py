e12f076a9b53373e3b0819d6ca95eacfde923896
WassCodeur <wachioubouraima56@gmail.com>
2023-05-10 01:07:15 +0000

    update perfome my code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,11 +16,25 @@ def do_deploy(archive_path):
     False if something went wrong
     """
     try:
+        archive_path_a = archive_path.split("versions/")
+        path_without_dir = "".join(archive_path_a)
+        archive_path_ext = path_without_dir.split(".tgz")
+        dir_name = "".join(archive_path_ext)
         put(archive_path, '/tmp/')
-        sudo("tar -xvzf /tmp/{}\
-                -C /data/web_static/releases/".format(archive_path))
-        sudo("rm  /tmp/{}".format(archive_path))
-        sudo("ln -sf /data/web_static/releases/ /data/web_static/current")
+        run("mkdir -p /data/web_static/releases/{}/".format(dir_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}\
+                ".format(path_without_dir, dir_name))
+        run("rm  /tmp/{}".format(path_without_dir))
+        run("mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}/\
+                ".format(dir_name, dir_name))
+        run("rm -rf /data/web_static/releases/{}/web_static\
+                ".format(dir_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/\
+                /data/web_static/current".format(dir_name))
+
+        print("New version deployed!")
         return True
     except Exception:
         return False
