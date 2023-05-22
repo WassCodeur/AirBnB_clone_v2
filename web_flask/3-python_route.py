c52f2700ad17fb12a359e0fc31664b2087b2074e
WassCodeur <wachioubouraima56@gmail.com>
2023-05-22 00:21:29 +0000

    setup my PYTHON route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask('__name__')
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Entry point - hello_hbnb
+
+    parameters : none
+
+    return : hello HBNB
+    """
+    return "hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Entry point - hbnb
+
+    parameter : none
+
+    return : HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Entry point - c_text
+
+    parameter : text
+
+    return : C text
+    """
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    Entry piont - python_text
+
+    parameters : text
+
+    return : Python text
+    """
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    """
+    check if the program is the main program
+    """
+    app.run(host='0.0.0.0', port=5000)
